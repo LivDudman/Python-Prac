9c7354281cb0f7f9a29b66aba9386aec2d6c4b92
Olivia Dudman <olivia2.dudman@live.uwe.ac.uk>
2019-06-22 20:34:04 +0100

    Add files via upload

---
+++ Charactor Input 2.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+print("OK " + name + ",")
+
+age = input("What year was you born? ")
+age = int(age)
+
+old = (age + 100)
+old = str(old)
+
+print("Did you know " + name + "that when you turn 100 years old it will be " + old)
